36a2b873cac6236b0d80e9f1b3fe72cfc87ed230
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-06-09 20:36:10 -0600

    format method

---
+++ python-basic.py
@@ -0,0 +1,8 @@
+print("Mohammed has {} balloons".format(27))
+# Mohammed has 27 balloons
+
+animal = "dog"
+action = "bite"
+print("Does your {} {}?".format(animal, action))
+# Does your dog bite?
+
